9dbe39b53a4cb924ee27dc64f3092f91741ed894
Harsh Kachhawa <harsh.kachhawa2002@gmail.com>
2024-03-09 13:07:02 +0530

    KTI-38:Minor Bugs Fixed

---
+++ utils/SentryUtils.ts
@@ -0,0 +1,18 @@
+import * as Sentry from "@sentry/react";
+export const initializeSentry = () => {
+  const dsn = import.meta.env.REACT_APP_SENTRY_DSN_PROD;
+  Sentry.init({
+    dsn,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+    ],
+    tracesSampleRate: 1,
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+};
