427a444d6b51bd46760b55e587e370ac7f73333f
Harsh Kachhawa <harsh.kachhawa2002@gmail.com>
2024-03-09 13:07:02 +0530

    KTI-38:Minor Bugs Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./Component/Header/Header";
-import{Footer} from "./Component/Footer/Footer"
+import { Footer } from "./Component/Footer/Footer";
 import * as Sentry from "@sentry/react";
-import{Home} from "./Component/Home/Home"
+import { Home } from "./Component/Home/Home";
 function App() {
-  
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-      <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
     </BrowserRouter>
-   
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './redux/Store.ts'
-import * as Sentry from "@sentry/react";
-Sentry.init({
-  dsn: "https://c7645ee440b184e8d2c25a35a34caef7@o4506870923460608.ingest.us.sentry.io/4506870929686528",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  tracesSampleRate: 0.3,
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0, 
-});
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/Store.ts";
+import { initializeSentry } from "../utils/SentryUtils.ts";
+if (import.meta.env.VITE_REACT_APP_ENV === "PROD") {
+  initializeSentry();
+}
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-     <Provider store={store}>
+    <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
