c6ef54e88004b95fc56618d30867ab993c1cb4dc
Harsh Kachhawa <harsh.kachhawa2002@gmail.com>
2024-03-09 14:11:46 +0530

    KTI:36 Minor Bugs Fixed

--- cypress/component/Header.cy.tsx
+++ cypress/component/Header.cy.tsx
@@ -2,15 +2,19 @@ import { Provider } from "react-redux";
 import { store } from "../../src/redux/Store";
 import { Header } from "../../src/Component/Header/Header";
 
-describe('<Header />', () => {
-  it('mounts', () => {
-    cy.viewport(1024, 700);
+describe("<Header />", () => {
+  
+  it("mounts", () => {
+    cy.viewport(
+      parseInt(`${import.meta.env.VITE_VIEWPORT_WIDTH}`),
+      parseInt(`${import.meta.env.VITE_VIEWPORT_HEIGHT}`)
+    );
     cy.mount(
       <Provider store={store}>
-      <Header />
+        <Header />
       </Provider>
-    )
-    cy.get('.main-heading').should('contain', 'Kickdrum');
-    cy.get('.sub-heading').should('contain', 'Internet Booking Engine');
-  })
+    );
+    cy.get(".main-heading").should("contain", "Kickdrum");
+    cy.get(".sub-heading").should("contain", "Internet Booking Engine");
+  });
 });
