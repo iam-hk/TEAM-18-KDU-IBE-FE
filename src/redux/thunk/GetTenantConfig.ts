d22361c1584d400dd42e9c5c5bbf59be600a7ee5
Harsh Kachhawa <harsh.kachhawa2002@gmail.com>
2024-03-18 12:11:28 +0530

    .env url changed

--- src/redux/thunk/GetTenantConfig.ts
+++ src/redux/thunk/GetTenantConfig.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const getTenantConfig = createAsyncThunk("getTenantConfig", async () => {
   const url = import.meta.env.VITE_REACT_APP_CONFIGURATION;
+  console.log("tenant url",url);
   try {
     const res = await fetch(url);
     const data = await res.json();
