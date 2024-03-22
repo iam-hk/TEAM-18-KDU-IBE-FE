28b569471d88fe58dd225f1ec2dbbf333c0452dc
ankush-r <ankushrauni@gmail.com>
2024-03-22 14:52:41 +0530

    added calender component

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -6,17 +6,33 @@ import { PropertyNameReducer } from "./PropertyNameSlice";
 import { PropertyConfigReducer } from "./PropertyConfigSlice";
 import { StepperReducer } from "./StepperSlice";
 import { FilterRoomReducer } from "./FilterRoomSlice";
+
+import { combineReducers } from "@reduxjs/toolkit";
+
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  currencyRate: CurrencyReducer,
+  searchRoomInfo: SearchRoomReducer,
+  tenantInfo: TenantReducer,
+  propertyNameInfo: PropertyNameReducer,
+  propertyConfigInfo: PropertyConfigReducer,
+  stepper: StepperReducer,
+  filterRoom: FilterRoomReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    currencyRate: CurrencyReducer,
-    searchRoomInfo: SearchRoomReducer,
-    tenantInfo: TenantReducer,
-    propertyNameInfo: PropertyNameReducer,
-    propertyConfigInfo: PropertyConfigReducer,
-    stepper:StepperReducer,
-    filterRoom:FilterRoomReducer
-  },
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
