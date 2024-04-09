ace6005f1a025ef1fdfcdebd05432b4be8a87713
Harsh Kachhawa <harsh.kachhawa2002@gmail.com>
2024-04-09 14:28:24 +0530

    Timer modified

--- src/Component/CheckoutPage/TimerComponent/TimerComponent.tsx
+++ src/Component/CheckoutPage/TimerComponent/TimerComponent.tsx
@@ -1,64 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { styled } from '@mui/system';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import { styled } from "@mui/system";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../redux/Store";
 
-const TimerDisplay = styled('div')({
-    display: 'flex',
-    alignItems: 'center',
-    gap: '8px',
-    fontSize: '1.8rem',
-    color: 'white',
-    backgroundColor: '#9794b7',
-    padding: '8px',
-    justifyContent: 'center',
+const TimerDisplay = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  gap: "8px",
+  fontSize: "1.8rem",
+  color: "white",
+  backgroundColor: "#5a52adb5",
+  padding: "8px",
+  justifyContent: "center",
 });
 
 const TimerIcon = styled(AccessAlarmIcon)({
-    fontSize: '2.0rem',
-    color: 'white',
+  fontSize: "2.0rem",
+  color: "white",
 });
 
 const CountdownTimer = () => {
-    const [timeLeft, setTimeLeft] = useState(600);
-    const navigate = useNavigate(); // Initialize useNavigate hook
+  const timeLeft = useSelector((state: RootState) => state.stepper.timeLeft);
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = Math.floor(timeLeft % 60);
 
-    useEffect(() => {
-        const storedStartTime = localStorage.getItem('startTime');
-        let startTime = storedStartTime ? parseInt(storedStartTime) : Date.now();
-        const remainingTime = 600 - ((Date.now() - startTime) / 1000);
-        setTimeLeft(remainingTime > 0 ? remainingTime : 0);
-        
-        const timerId = setInterval(() => {
-            setTimeLeft((prevTimeLeft) => {
-                if (prevTimeLeft <= 0) {
-                    clearInterval(timerId);
-                    localStorage.removeItem('startTime');
-                    // Use navigate('/') to navigate to '/'
-                    navigate('/');
-                    return 0;
-                }
-                return prevTimeLeft - 1;
-            });
-        }, 1000);
-
-        if (!storedStartTime) {
-            localStorage.setItem('startTime', startTime.toString());
-        }
-
-        return () => clearInterval(timerId);
-    }, [navigate]); // Add navigate to dependencies array to avoid warning
-
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = Math.floor(timeLeft % 60);
-
-    return (
-        <TimerDisplay>
-            <TimerIcon />
-            {minutes} Minutes {seconds} Seconds left!
-        </TimerDisplay>
-    );
+  return (
+    <TimerDisplay>
+      <TimerIcon />
+      {minutes} Minutes {seconds} Seconds left!
+    </TimerDisplay>
+  );
 };
 
 export default CountdownTimer;
-
