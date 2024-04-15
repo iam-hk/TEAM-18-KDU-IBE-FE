f1b051dff884f78aac4f204bbd140242ee1f6228
Harsh Kachhawa <harsh.kachhawa2002@gmail.com>
2024-04-15 13:57:25 +0530

    Changes made in sentry

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/Store.ts";
 import { initializeSentry } from "../src/utils/SentryUtils.ts";
-if (import.meta.env.VITE_REACT_APP_ENV === "PROD") {
+// if (import.meta.env.VITE_REACT_APP_ENV === "PROD") {
   initializeSentry();
-}
+// }
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
     <Provider store={store}>
